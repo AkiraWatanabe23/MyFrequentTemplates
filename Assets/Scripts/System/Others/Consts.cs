d5cc36328186177ce557c02a452f294afededfc6
AkiraWatanabe23 <a0323w@gmail.com>
2023-09-12 01:46:34 +0900

    [add] 定数ファイル、SingletonMonoBehaviour

---
+++ Assets/Scripts/System/Others/Consts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Constants
+{
+    /// <summary> 定数管理ファイル（プロジェクト毎に必要があれば追記する） </summary>
+    public static class Consts
+    {
+        public static readonly Dictionary<SceneNames, string> Scenes = new()
+        {
+            [SceneNames.Title] = "TitleScene",
+            [SceneNames.Home] = "HomeScene",
+            [SceneNames.MainGame] = "MainGameScene",
+        };
+    }
+}
+
+#region enum一覧
+public enum SceneNames
+{
+    Title,
+    Home,
+    MainGame,
+}
+#endregion
