3cd7934e5548c70de0a238bc0cc392e404f3bb8f
AkiraWatanabe23 <a0323w@gmail.com>
2023-09-12 17:04:15 +0900

    [add] IPAddressをjsonから読み込むためのクラス

--- Assets/Scripts/System/JsonDataReader.cs
+++ Assets/Scripts/System/JsonDataReader.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 
-/// <summary> Jsonファイルを読み込みを行うためのクラス </summary>
+/// <summary> jsonファイルを読み込みを行うためのクラス </summary>
 /// <typeparam name="T"> 読み込んだデータを保存する型 </typeparam>
 public static class JsonDataReader<T>
 {
-    private static T _loadData = default;
-
-    public static T LoadData => _loadData;
-
     /// <summary> jsonファイルの読み込み </summary>
-    public static void JsonRead(TextAsset textAsset)
+    public static T JsonRead(TextAsset textAsset)
     {
+        //引数に渡されたjsonファイルを文字列型に変換する
         var jsonText = textAsset.ToString();
-        _loadData = JsonUtility.FromJson<T>(jsonText);
+#if UNITY_EDITOR
+        Debug.Log(jsonText);
+#endif
+
+        //文字列に変換したjsonデータを指定された型に置き換える
+        T loadData = JsonUtility.FromJson<T>(jsonText);
+
+        return loadData;
     }
 }
---
+++ Assets/Scripts/System/Network/IPAddressHolder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+/// <summary> jsonファイルから使用するマシンのIPAddressを読み込む（ネットワーク通信の際に使用する） </summary>
+public class IPAddressHolder : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset _textAsset = default;
+
+    private MachinesData _machinesData = default;
+
+    public List<IPAddress> IPAddresses { get; private set; }
+
+    /// <summary> jsonファイルの読み込み、IPAddressへの変換（最初だけ呼び出す） </summary>
+    public void IPAddressInitialize()
+    {
+        if (!_textAsset) { Debug.LogError("jsonファイルの設定がされていません"); return; }
+
+        _machinesData = JsonDataReader<MachinesData>.JsonRead(_textAsset);
+
+        IPAddresses = new();
+        foreach (var machineData in _machinesData.Machines) { IPAddresses.Add(IPAddress.Parse(machineData)); }
+    }
+}
+
+/// <summary> jsonファイルのデータを保存しておくためのクラス </summary>
+[Serializable]
+public class MachinesData
+{
+    //以下変数名はjsonファイルに合わせるようにする
+    public string MachineName1;
+    public string MachineName2;
+    public string MachineName3;
+    public string MachineName4;
+
+    private string[] _machines = default;
+    public string[] Machines
+    {
+        get
+        {
+            _machines ??= new string[] { MachineName1, MachineName2, MachineName3, MachineName4 };
+            return _machines;
+        }
+    }
+}
